6e3fd26e3dda017ec4884997f4f6deefad5be29f
Jeremy <jerorr4@yahoo.ca>
2023-03-11 20:29:47 -0500

    Added Created by name, and added a quit button

--- ASLTracker.py
+++ ASLTracker.py
@@ -7,12 +7,9 @@ import numpy as np
 import math
 import time
 
-
 cap = cv2.VideoCapture(0)
 detector = HandDetector(maxHands=1) #max hands
 classifier = Classifier("Model/keras_model.h5", "Model/labels.txt")
-
-
 
 
 offset = 20 #Create offset for smaller box
@@ -23,12 +20,25 @@ counter = 0
 
 labels = ["A","B","C"]
 
+#Quit Key
+def quit_key_pressed():
+    # Wait for a key press
+    key = cv2.waitKey(1) & 0xFF
+
+    # If the "q" key was pressed, return True
+    if key == ord('q'):
+        return True
+
+    return False
 
 while True:
     success, img = cap.read()
     imgOutput = img.copy()
     hands, img = detector.findHands(img)
 
+    if quit_key_pressed():
+            break
+    
     if hands:
         hand = hands[0]
         x,y,w,h = hand['bbox'] #Create the smaller box for our hand
@@ -47,8 +57,8 @@ while True:
             wGap = math.ceil((300-wCal)/2)
             imgWhite[:, wGap:wCal+wGap] = imgResize #putting white as backgound
             
-            prediction, index = classifier.getPrediction(imgWhite, draw=False)
-            print(prediction,index)
+            prediction, index = classifier.getPrediction(imgWhite, draw=True)
+            #print(prediction,index)
 
         else:
             k = imgSize/w
@@ -58,21 +68,26 @@ while True:
             hGap = math.ceil((imgSize-hCal)/2)
             imgWhite[hGap:hCal+hGap, :] = imgResize #putting white as backgound
             
-            prediction, index = classifier.getPrediction(imgWhite, draw=False)
-
+            prediction, index = classifier.getPrediction(imgWhite, draw=True)
         
-        cv2.putText(imgOutput, labels[index],(x,y-20),cv2.FONT_HERSHEY_COMPLEX,2,(255,0,255),2)
+        cv2.putText(imgOutput, labels[index],(x,y-20), cv2.FONT_HERSHEY_DUPLEX,2,(255,0,255),2)
         #cv2.rectangle(imgOutput,(x-offset,y-offset), (x+w+offset,y+h+offset), (255,0,255),4) rectangle around hand
 
         cv2.imshow("ImageCrop",imgCrop)
         cv2.imshow("ImageWhite",imgWhite)
 
 
-        cv2.putText(img, "Jeremy Orr ©", (100, 100), cv2.FONT_HERSHEY_COMPLEX,2,(0,0,0),2)
+        #Drawing the creator code
+        CreatorText = "Created by Jeremy Orr"
+        #cv2.rectangle(imgOutput, (0,400), (50,460), (0, 255, 0), thickness=10)
+        cv2.putText(imgOutput, CreatorText, (0, 475), cv2.FONT_HERSHEY_DUPLEX,0.9,(255,255,255),1)        
 
 
-    cv2.imshow("Image",imgOutput) #on the image output now chnage to img for other thing
+        #Exit key code
+        if quit_key_pressed():
+            break
+
+
+    cv2.imshow("Image",imgOutput) #Show new image without backend.
     cv2.waitKey(1)
-
-
 
